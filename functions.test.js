8f05ba04612e83636c3d3e93774e6fb994d8f815
Laura Juliana Orjuela Galicia <laura.orjuela@globant.com>
2024-02-23 20:34:20 -0500

    testing

--- functions.test.js
+++ functions.test.js
@@ -3,11 +3,13 @@ const functions = require("./functions.js");
 describe('multiply function', () => {
   it('should be return multiple when I sent two numbers', () => {
     //ARRANGE
-
+    const a = 9;
+    const b = 4;
+  
     //ACT
-
+    const result = functions.multiplicar(a, b);
     //ASSERT
-
+    expect(result).toBe(36);
   })
 });
 
@@ -17,8 +19,11 @@ describe('isNull function', () => {
     //ARRANGE
 
     //ACT
-
+    const result = functions.isNull();
+    
     //ASSERT
+    expect(result).toBeNull();
+    
 
   });
 });
@@ -26,21 +31,26 @@ describe('isNull function', () => {
 describe('checkTruthy function', () => {
   it('should be return true when I sent true', () => {
     //ARRANGE
+    const valor = true;
 
     //ACT
-
+    const result = functions.checkTruthy(valor);
+    
     //ASSERT
-
+    expect(result).toBe(true);
   });
 })
 
 describe('addLastName function', () => {
   it('should be return Pepito Perez when I sent Perez', () => {
     //ARRANGE
+    const apellido = "Perez";
 
     //ACT
+    const result = functions.addLastName("Pepito", apellido);
 
     //ASSERT
+    expect(result).toBe("Pepito Perez");
 
   });
 })
